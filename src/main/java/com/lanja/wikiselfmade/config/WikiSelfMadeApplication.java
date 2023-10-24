c8cf4e4fe2d6b9cfa10b292246f3b3355baa476c
gavinz915 <jianzheng_915@163.com>
2023-10-24 15:52:21 +0800

    测试/hello接口同时更改启动类位置及扫描包路径

--- src/main/java/com/lanja/wikiselfmade/config/WikiSelfMadeApplication.java
+++ src/main/java/com/lanja/wikiselfmade/config/WikiSelfMadeApplication.java
@@ -1,11 +1,13 @@
-package com.lanja.wikiselfmade;
+package com.lanja.wikiselfmade.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
+@ComponentScan("com.lanja")
 @SpringBootApplication
 public class WikiSelfMadeApplication {
 
