ca259f3f8992190b6b7d61271c2814fbf635dc21
gavinz915 <jianzheng_915@163.com>
2023-10-24 13:31:10 +0800

    项目初始状态

---
+++ src/main/java/com/lanja/wikiselfmade/WikiSelfMadeApplication.java
@@ -0,0 +1,13 @@
+package com.lanja.wikiselfmade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikiSelfMadeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WikiSelfMadeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lanja/wikiselfmade/WikiSelfMadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lanja.wikiselfmade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiSelfMadeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
